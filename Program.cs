a4ba9df89d6c5bc4c856dcb8feae59aa175c54b1
Joseph K Justus <justuskasyoki57@gmail.com>
2022-05-04 16:21:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using PesalinkLiveTester.TranscationClasses;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesalinkLiveTester
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            CreditTransfer();
+        }
+        public static string CreditTransfer()
+        {
+            string Response = null;
+
+            var commanddata = new Credit_CreditTransfer()
+            {
+                OriginalmessageId = "",
+                DebtorPhonNb = "",
+                CdtrBankCode = "",
+                DbtrName = "",
+                DbtrAcct = "",
+                DbtrAcctName = "",
+                amount = "",
+                EndtoEndId = ""
+
+            };
+            var Body = JsonConvert.SerializeObject(commanddata);
+
+            var client = new RestClient("");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            var body = Body;
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            Response = response.Content;
+            return Response;
+        }
+        public static string AccountVerification()
+        {
+            string Response = null;
+
+            var commanddata = new AccountVerification()
+            {
+               AccountNo ="",
+               BackCode =""
+
+            };
+            var Body = JsonConvert.SerializeObject(commanddata);
+
+            var client = new RestClient("");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            var body = Body;
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            Response = response.Content;
+            return Response;
+        }
+    }
+}
---
+++ TranscationClasses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesalinkLiveTester.TranscationClasses
+{
+    public class Credit_CreditTransfer
+    {
+        public string OriginalmessageId { get; set; }
+        public string DebtorPhonNb { get; set; }
+        public string CdtrBankCode { get; set; }
+        public string DbtrName { get; set; }
+        public string DbtrAcct { get; set; }
+        public string DbtrAcctName { get; set; }
+        public string amount { get; set; }
+        public string EndtoEndId { get; set; }
+    }
+    public class AccountVerification
+    {
+        public string BackCode { get; set; }
+        public string AccountNo { get; set; }
+    }
+}
